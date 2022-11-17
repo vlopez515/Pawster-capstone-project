7f7fb76d1007f6fc3c65123dadcef72589312285
Victor E Lopez <velbx51594@macbook-air.mynetworksettings.com>
2022-11-16 19:54:25 -0500

    initial commit v2

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,30 +1,33 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
-const API = process.env.REACT_APP_API_URL;
+// const API = process.env.REACT_APP_API_URL;
 
-console.log(API);
+
 function App() {
-  const [days, setDays] = useState([]);
-  useEffect(() => {
-    axios
-      .get(`${API}/test`)
-      .then(
-        (response) => {
-          setDays(response.data);
-        },
-        (error) => console.log("get", error)
-      )
-      .catch((c) => console.warn("catch", c));
-  }, []);
+  // const [days, setDays] = useState([]);
+  // useEffect(() => {
+  //   axios
+  //     .get(`${API}/test`)
+  //     .then(
+  //       (response) => {
+  //         setDays(response.data);
+  //       },
+  //       (error) => console.log("get", error)
+  //     )
+  //     .catch((c) => console.warn("catch", c));
+  // }, []);
+  // return (
+  //   <div>
+  //     <ul>
+  //       {days.map((day) => (
+  //         <li key={day.name}>{day.name}</li>
+  //       ))}
+  //     </ul>
+  //   </div>
+  // );
   return (
-    <div>
-      <ul>
-        {days.map((day) => (
-          <li key={day.name}>{day.name}</li>
-        ))}
-      </ul>
-    </div>
-  );
+    <div>hello</div>
+  )
 }
 
 export default App;
