a83bcc9dc36a2f7935d13fd69bdbda33d70f9700
Victor E Lopez <velbx51594@macbook-air.mynetworksettings.com>
2022-11-20 11:16:14 -0500

    controllers

--- back-end/app.js
+++ back-end/app.js
@@ -2,35 +2,29 @@
 const cors = require("cors");
 const express = require("express");
 
-// CONFIGURATION
+
 const app = express();
 
-// MIDDLEWARE
-app.use(cors());
-app.use(express.json()); // Parse incoming JSON
 
-// ROUTES
+app.use(cors());
+app.use(express.json()); 
+
+
 app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
 
-/////////////////////////////////////
-// REMOVE AFTER SUCCESSFUL DEPLOYMENT
-/////////////////////////////////////
+
 const db = require("./db/dbConfig.js");
 
-app.get("/test", async (req, res) => {
+app.get("/data", async (req, res) => {
   try {
-    const allDays = await db.any("SELECT * FROM test");
+    const allDays = await db.any("SELECT * FROM data");
     res.json(allDays);
   } catch (err) {
     res.json(err);
   }
 });
 
-/////////////////////////////////////
-// REMOVE AFTER SUCCESSFUL DEPLOYMENT
-/////////////////////////////////////
 
-// EXPORT
 module.exports = app;
