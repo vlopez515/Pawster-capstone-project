dd5a381f8192b7c23e34da8a65d3f535be6e499c
vlopez515 <vlopez2124@gmail.com>
2022-12-04 16:02:29 -0500

    seed changes2

--- back-end/db/prod_seed.sql
+++ back-end/db/prod_seed.sql
@@ -6,7 +6,7 @@ VALUES
 
 INSERT INTO shelters (name, address, business_hours, phone_number, email) VALUES 
 ('Tuff Tails Animal Rescue', 'P.O. Box 117, Levittown, NY 11756', null, null,'info@tufftails.org'),
-('P.A.N.T.', 'P.O. Box 143, Salt Point, NY 12578','845-486-7075 ext.1',null, 'adoption@PANT.org'),
+('P.A.N.T.', 'P.O. Box 143, Salt Point, NY 12578', null, '845-486-7075 ext.1', 'adoption@PANT.org'),
 ('Husky Savers, Inc.', 'Honeoye Falls, NY 14472', null, '585-576-1724', 'hskysavr1@gmail.com'),
 ('P.L.U.T.O. (Pet Lovers United Together as One)','PO Box 140889, Staten Island, NY 10314',null,null,'plutorescue@aol.com'),
 ('Estherville Animal Shelter','100 Russell Road, Greenfield Center, NY 12833', null, '518-893-7451', 'Esthervilleanimalshelter54@gmail.com'),
