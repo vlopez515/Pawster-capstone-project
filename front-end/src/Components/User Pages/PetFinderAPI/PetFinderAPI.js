1606bd0711981f821abd22b7118b0bb855a0f49e
vlopez515 <vlopez2124@gmail.com>
2022-12-12 14:00:46 -0500

    swipePageStyleChanges

--- front-end/src/Components/User Pages/PetFinderAPI/PetFinderAPI.js
+++ front-end/src/Components/User Pages/PetFinderAPI/PetFinderAPI.js
@@ -43,14 +43,37 @@ export default function PetFinderAPI() {
 
   return (
     <div className="PetFinderAPI_card">
-        <h3>Swipe Right to like &#10084;</h3>
+      <h3>Swipe Right to like &#10084;</h3>
       <button
         className="PetFinderAPI_userLikedPet_Button"
         onClick={() => navigate("/user/1/pets")}
       >
-       Click here for &#10084; pets
+        <a
+          href="#_"
+          class="relative inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-black-600 transition duration-300 ease-out border-2 border-black rounded-full shadow-md group"
+        >
+          <span class="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-yellow-500 group-hover:translate-x-0 ease">
+            <svg
+              class="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M14 5l7 7m0 0l-7 7m7-7H3"
+              ></path>
+            </svg>
+          </span>
+          <span class="absolute flex items-center justify-center w-full h-full text-black-500 transition-all duration-300 transform group-hover:translate-x-full ease">
+            Liked Pets
+          </span>
+          <span class="relative invisible">Liked Pets</span>
+        </a>
       </button>
-
       <TinderCards animals={animals} />
     </div>
   );
