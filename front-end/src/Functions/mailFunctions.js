7bd44921ea8f92897ee8d42c9c1fc12f75522ce1
ziqian pan <ziqianpan1998@gmail.com>
2022-12-12 04:48:44 -0500

    commented out email function

--- front-end/src/Functions/mailFunctions.js
+++ front-end/src/Functions/mailFunctions.js
@@ -1,33 +1,33 @@
 
 // // ------------ Mail Functions ------------
 
-export function sendMail(event) {
-  event.preventDefault()
-  console.log(event)
-  let email = {
-    fullName: event.target[0].value,
-    email: event.target[1].value,
-    subject: event.target[2].value,
-    message: event.target[3].value
-  }
+// export function sendMail(event) {
+//   event.preventDefault()
+//   console.log(event)
+//   let email = {
+//     fullName: event.target[0].value,
+//     email: event.target[1].value,
+//     subject: event.target[2].value,
+//     message: event.target[3].value
+//   }
 
-  console.log(email)
+//   console.log(email)
 
-  const serviceID = "service_eun4ifs";
-  const templateID = "template_3ytz8si";
+//   const serviceID = "service_eun4ifs";
+//   const templateID = "template_3ytz8si";
 
-  "emailjs function works but for some reason throws an error when the app is rendered. why?"
-  emailjs.send(serviceID, templateID, email)
-  .then((res) => {
-      event.target[0].value = "";
-      event.target[1].value = "";
-      event.target[2].value = "";
-      event.target[3].value = "";
-      console.log(res);
-      alert("Thank You! Your submission was sent.")
-  }
-  )
-  .catch((err) => console.log(err))
-}
+//   "emailjs function works but for some reason throws an error when the app is rendered. why?"
+//   emailjs.send(serviceID, templateID, email)
+//   .then((res) => {
+//       event.target[0].value = "";
+//       event.target[1].value = "";
+//       event.target[2].value = "";
+//       event.target[3].value = "";
+//       console.log(res);
+//       alert("Thank You! Your submission was sent.")
+//   }
+//   )
+//   .catch((err) => console.log(err))
+// }
 
 
