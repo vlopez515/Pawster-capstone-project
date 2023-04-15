d9dea2a3daa3ce3fe48d94a0f7752a6e172f83d7
vlopez515 <vlopez2124@gmail.com>
2023-04-15 15:55:05 -0400

    db.config changes

--- back-end/db/dbConfig.js
+++ back-end/db/dbConfig.js
@@ -2,25 +2,15 @@
 const pgp = require("pg-promise")();
 require("dotenv").config();
 
-const { DATABASE_URL, PG_HOST, PG_PORT, PG_DATABASE, PG_USER, PG_PASSWORD } =
-  process.env;
-// https://github.com/vitaly-t/pg-promise/wiki/Connection-Syntax#configuration-object
-const cn = DATABASE_URL
-  ? {
-      connectionString: DATABASE_URL,
-      max: 30,
- // this key value is only required for heroku deployment
-//       ssl: {
-//         rejectUnauthorized: false,
-//       },
-    }
-  : {
-      host: PG_HOST,
-      port: PG_PORT,
-      database: PG_DATABASE,
-      user: PG_USER,
-      password: PG_PASSWORD,
-    };
+
+const databaseUrl = process.env.DATABASE_URL;
+
+const cn = {
+  connectionString: databaseUrl,
+  allowExitOnIdle: true,
+  max: 30,
+};
+
 
 const db = pgp(cn);
 
