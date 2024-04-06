ddcf9fda36b16284e4746a6dab51526ae4cd87c7
vlopez515 <vlopez2124@gmail.com>
2024-04-06 01:20:06 -0400

    deleted controllers

--- back-end/app.js
+++ back-end/app.js
@@ -6,18 +6,12 @@ const app = express();
 // CONTROLLERS
 const usersController = require("./controllers/usersController");
 const petsController = require("./controllers/petsController");
-const sheltersController = require("./controllers/sheltersController");
-const messageController = require("./controllers/messageController");
 
 app.use(cors());
 app.use(express.json()); 
 
-
 app.use("/users", usersController);
 app.use("/pets", petsController);
-app.use("/shelters", sheltersController);
-app.use("/messages", messageController);
-
 
 app.get("/", (req, res) => {
   res.send("Hello,!");
@@ -27,6 +21,5 @@ app.get("/", (req, res) => {
 app.get("*", (req, res) => {
   res.status(404).send("page not found")
 })
-
 
 module.exports = app;
